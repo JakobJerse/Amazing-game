c6020897245d0f6a6872302c39b19b0908a6652b
JakobJerse <jakob.jerse@gmail.com>
2022-11-22 23:07:02 +0100

    first working version

---
+++ bin/my_obj2json.js
@@ -0,0 +1,122 @@
+const fs = require('fs');
+
+const data = fs.readFileSync(process.argv[2], 'utf8');
+const lines = data.split('\n');
+
+const vertices = []
+let vertexIx = 0
+
+const normals = []
+let normalIx = 0
+
+const texsels = []
+let texselIx = 0
+
+const objects = []
+let objectIx = -1
+
+let object = []
+
+const materialObstaja = []
+const stevilkaMateriala = []
+
+lines.forEach(function (line, lineIndex) {
+    //console.log(item, index);
+    //console.log(item[0])
+    elements = line.split(/[ ]+/)
+    // if(index == 3 || index == 4) {
+    //     console.log(elements)
+    // }
+    if (elements[0] === "usemtl") {
+        if (materialObstaja.includes(elements[1])) {
+            let indexMateriala = -1
+            for (let x = 0; x < materialObstaja.length; x++) {
+                if (materialObstaja[x] == elements[1]) {
+                    indexMateriala = x
+                    break
+                }
+            }
+            stevilkaMateriala.push(indexMateriala)
+        } else {
+            materialObstaja.push(elements[1])
+            stevilkaMateriala.push(materialObstaja.length - 1)
+        }
+    }
+    if (elements[0] === "v") {
+        vertices.push([Number.parseFloat(elements[1]), Number.parseFloat(elements[2]), Number.parseFloat(elements[3])])
+        vertexIx++
+    }
+    if (elements[0] === "vn") {
+        normals.push([Number.parseFloat(elements[1]), Number.parseFloat(elements[2]), Number.parseFloat(elements[3])])
+        normalIx++
+    }
+    if (elements[0] === "vt") {
+        texsels.push([Number.parseFloat(elements[1]), Number.parseFloat(elements[2])/*, Number.parseFloat(elements[3])*/])
+        texselIx++
+    }
+    if (elements[0] === "o") {
+        if (objectIx != -1) {
+            objects.push(object)
+            //console.log(objects)
+            object = object.map((x) => x);
+            object.length = 0
+            //console.log(objects)
+        }
+        objectIx++
+    }
+    if (elements[0] === "f") {  // lahko so 3, 4 ali vec ogljisc   +   lahko so podani indeksi brez texsel indeksa (takrat:  IndeXnumber//IndeXnumber)
+        // console.log(elements[1].split(/[/]+/))
+        // console.log(elements[2].split(/[/]+/))
+        // console.log(elements[3].split(/[/]+/))
+        const polygon = {}
+        const vertexIndices = []
+        const texselIndices = []
+        const normalIndices = []
+        elements.forEach(function (element, elementIndex) {
+            if (elementIndex === 0) {
+                // do nothing ("f")
+            } else {
+                indexTuple = element.split(/[/]+/)
+                if (indexTuple.length == 2) {   // ni texsel informacije     vertex//normal
+                    vertexIndices.push(Number.parseInt(indexTuple[0]))
+                    normalIndices.push(Number.parseInt(indexTuple[1]))
+                } else {                       // vse informacije    vertex/texsel/normal
+                    vertexIndices.push(Number.parseInt(indexTuple[0]))
+                    texselIndices.push(Number.parseInt(indexTuple[1]))
+                    normalIndices.push(Number.parseInt(indexTuple[2]))
+                }
+            }
+        });
+        polygon.vertexIndices = vertexIndices
+        if (texselIndices.length > 0) {
+            polygon.texselIndices = texselIndices
+        }
+        polygon.normalIndices = normalIndices
+        object.push(polygon)
+        // console.log("vertexIX")
+        // console.log(vertexIndices)
+        // console.log("texselIX")
+        // console.log(texselIndices)
+        // console.log("normalIX")
+        // console.log(normalIndices)
+        //console.log(polygon)
+    }
+    if (lineIndex == lines.length - 1) {  // shranimo se zadnji objekt, ker ni vec "g" oznake
+        objects.push(object)
+        //console.log("zadnji objekt shranjen")
+    }
+});
+
+map = { vertices: vertices, normals: normals, texsels: texsels, objects: objects }
+
+// console.log(vertices)
+// console.log(normals)
+// console.log(texsels)
+//console.log(objects)
+
+
+const motherString = JSON.stringify(map)
+//console.log(motherString)
+
+fs.writeFileSync('fakof.json', motherString);
+//fs.writeFileSync('hatersGonnaHatePotatosGonnaPotate.json', JSON.stringify({materiali:stevilkaMateriala}));
---
+++ game/SceneBuilder.js
@@ -0,0 +1,85 @@
+import { Mesh } from './Mesh.js';
+
+import { Node } from './Node.js';
+import { Model } from './Model.js';
+import { Camera } from './Camera.js';
+
+import { Scene } from './Scene.js';
+
+export class SceneBuilder {
+
+    constructor(spec) {
+        this.spec = spec;
+    }
+
+    createNode(spec) {
+        switch (spec.type) {
+            case 'camera': return new Camera(spec);
+
+            case 'object': {
+                const vertices1D = []
+                const normals1D = []
+                const texels1D = []
+                const indices1D = []
+                const mesh = this.spec.meshes[spec.mesh]
+                //console.log(mesh)
+                //console.log(mesh.objects[0].length)
+                let counter = 0
+                // handle vertices
+                for (let index = 0; index < mesh.objects[0].length; index++) {
+                    const triangle = mesh.objects[0][index]
+                    const triangle_indeces_array = triangle.vertexIndices
+                    for (let i = 0; i < 3; i++) {
+                        const vertex_vector = mesh.vertices[triangle_indeces_array[i] - 1]
+                        for (let j = 0; j < 3; j++) {
+                            vertices1D.push(vertex_vector[j])
+
+                        }
+                        indices1D.push(counter)
+                        counter++
+                    }
+                }
+                console.log(indices1D)
+                // handle normals
+                for (let index = 0; index < mesh.objects[0].length; index++) {
+                    const triangle = mesh.objects[0][index]
+                    const triangle_indeces_array = triangle.normalIndices
+                    for (let i = 0; i < 3; i++) {
+                        const normal_vector = mesh.normals[triangle_indeces_array[i] - 1]
+                        for (let j = 0; j < 3; j++) {
+                            normals1D.push(normal_vector[j])
+                        }
+                    }
+                }
+
+                // handle texels
+                for (let index = 0; index < mesh.objects[0].length; index++) {
+                    const triangle = mesh.objects[0][index]
+                    const triangle_indeces_array = triangle.texselIndices
+                    for (let i = 0; i < 3; i++) {
+                        const texel_vector = mesh.texsels[triangle_indeces_array[i] - 1]
+                        for (let j = 0; j < 2; j++) {
+                            texels1D.push(texel_vector[j])
+                        }
+                    }
+                }
+
+
+                const mesh1 = new Mesh({ vertices: vertices1D, texcoords: texels1D, normals: normals1D, indices: indices1D });
+                console.log(mesh1)
+                //console.log(spec)
+                //console.log(this.spec.meshes[spec.mesh])
+                const texture = this.spec.textures[spec.texture];
+                return new Model(mesh1, texture, spec);
+            }
+            default: return new Node(spec);
+        }
+    }
+
+    build() {
+        let scene = new Scene();
+        this.spec.nodes.forEach(spec => scene.addNode(this.createNode(spec)));
+        return scene;
+    }
+
+}
